ca15b9f6c9d8f9d6b1ec51a6322d0beac69c34e2
Louie Camacho <louiecamacho@gmail.com>
2026-01-14 13:57:38 -0600

    vault secured git status git status

---
+++ index.js
@@ -0,0 +1,25 @@
+let code1 = ``;
+code1 = 7+3;
+
+console.log(code1)
+
+
+
+const code2 = 90/2 - 5;
+
+console.log(code2);
+
+
+
+const code3 = 40 - 5%2;
+
+console.log(code3);
+
+
+const message = `The vault has been secured. The combination is:`;
+
+const codeA = code1+"-"+code2+"-"+code3;
+const codeB = `${code1}-${code2}-${code3}`;
+
+console.log(codeA);
+console.log(codeB);
